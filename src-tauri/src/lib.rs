e42e7dc3ae978c99a3604ec45dbe8a8b32c7376b
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-03-31 18:20:55 +0900

    プロセスの起動、keyの取得、プロセスの停止

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,16 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::sync::{Arc, Mutex};
+
+mod listen_keybord;
+use listen_keybord::AppState;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState { // AppStateを手動で初期化する必要がある、そのためArcやMutexを使っている
+            listener_process: Arc::new(Mutex::new(None)),
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![listen_keybord::start_listening, listen_keybord::stop_listening])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
