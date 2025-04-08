cbdcff2c322bf4e8c5d10e8154af681b0e790d97
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-04-08 18:05:21 +0900

    起動時に画像と文字列を保存するディレクトリ、ファイルを作成

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -12,18 +12,18 @@ pub struct AppState {
     pub listener_process: Arc<Mutex<Option<Child>>>, // マウス位置取得のバイナリクレートのプロセス
 }
 
+
 pub mod app_paths{
     use super::*; // app_pathsモジュールの外側でArc, Mutex, Lazy等をuseしているから、このモジュール内にimprtする必要がある
 
     pub static APP_DATA_PATH: Lazy<Arc<Mutex<Option<PathBuf>>>> = Lazy::new(|| Arc::new(Mutex::new(None)));
     pub static FILE_NAME:&str = "list.json";
+    pub static IMAGE_DIR:&str = "images";
 }
 
 
-
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Entry {
-    spell: String,
-    path: String,
+    pub spell: String,
+    pub path: String,
 }
