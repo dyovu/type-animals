52aef89df8b721d900df487f4c6308c42ce13a3c
Taku-Sashima <131934396+Taku-Sashima@users.noreply.github.com>
2025-04-15 19:07:24 +0900

    キーの判定、更新を実装

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,31 +1,123 @@
-use std::path::{PathBuf};
-use std::process::{Child};
+use std::path::PathBuf;
+use std::process::Child;
 use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
-
+use std::collections::{HashMap, VecDeque};
 
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-
+// マウス位置取得のバイナリクレートのプロセス
 pub struct AppState {
-    pub listener_process: Arc<Mutex<Option<Child>>>, // マウス位置取得のバイナリクレートのプロセス
+    pub listener_process: Arc<Mutex<Option<Child>>>,
 }
 
+impl AppState {
+    pub fn new() -> Self {
+        AppState {
+            listener_process: Arc::new(Mutex::new(None)),
+        }
+    }
 
-pub mod app_paths{
-    use super::*; // app_pathsモジュールの外側でArc, Mutex, Lazy等をuseしているから、このモジュール内にimprtする必要がある
+    pub fn get_listener_process(&self) -> &Arc<Mutex<Option<Child>>> {
+        &self.listener_process
+    }
+}
 
+pub static APP_STATE: Lazy<AppState> = Lazy::new(|| AppState::new());
+
+
+// アプリケーション全体のパスと、JSONのファイル名、画像ディレクトリ名
+// imiagesディレクトリ内のファイル名についてはJSONファイルのpathを参照する
+pub mod app_paths {
+    use super::*;
+
+    pub const JSON_FILE: &str = "list.json";  
+    pub const IMAGE_DIR: &str = "images";
     pub static APP_DATA_PATH: Lazy<Arc<Mutex<Option<PathBuf>>>> = Lazy::new(|| Arc::new(Mutex::new(None)));
-    pub static FILE_NAME:&str = "list.json";
-    pub static IMAGE_DIR:&str = "images";
+
+    pub fn initialize_app_data_path(app_dir: PathBuf) {
+        let mut path = APP_DATA_PATH.lock().unwrap();
+        *path = Some(app_dir);
+    }
+
+    pub fn get_json_path() -> PathBuf {
+        let path = APP_DATA_PATH.lock().unwrap();
+        path.as_ref().expect("アプリケーションのパスが初期化されていません").join(JSON_FILE)
+    }
 }
 
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Entry {
-    pub spell: String,
-    pub path: String,
+// start_listeningが呼ばれたらカウントをリセットして、initialize_key_countを呼び出す
+pub mod key_counts{
+    use super::*;
+
+    static KEY_COUNT: Lazy<Arc<Mutex<HashMap<String, VecDeque<char>>>>> = Lazy::new(||Arc::new(Mutex::new(HashMap::new())));
+
+    pub fn initialize_key_count() {
+        let json    = json_data::JSON_DATA.lock().unwrap();
+        let mut data = KEY_COUNT.lock().unwrap();
+
+        for (spell, _) in json.iter() {
+            let mut vec = VecDeque::new();
+            for c in spell.chars() {
+                vec.push_back(c);
+            }
+            data.insert(spell.clone(), vec);
+        }
+        println!("initialized key_count: {:?}", data);
+    }
+
+    pub fn reset_key_count() {
+        let mut data = KEY_COUNT.lock().unwrap();
+        data.clear();
+        println!("key_count reset");
+    }
+
+    pub fn get_keycount() -> HashMap<String, VecDeque<char>> {
+        let data = KEY_COUNT.lock().unwrap();
+        data.clone()
+    }
+
+    pub fn pop_and_refill(key:&str) -> bool{
+        let mut data = KEY_COUNT.lock().unwrap();
+        let dq = data.entry(key.to_string()).or_insert_with(VecDeque::new);
+        dq.pop_front();
+
+        if dq.is_empty() {
+            // キーの各文字をVecDequeに追加
+            for c in key.chars() {
+                dq.push_back(c);
+            }
+            return true;
+        }
+        return false;
+    }
 }
 
+
+pub mod json_data{
+    use super::*;
+
+    pub static JSON_DATA: Lazy<Arc<Mutex<HashMap<String, String>>>> = Lazy::new(||Arc::new(Mutex::new(HashMap::new())));
+
+    // jsonファイル作成時と値を変更、追加した際に呼び出す
+    pub fn initialize_json_data() {
+        let json_path: PathBuf = app_paths::APP_DATA_PATH.lock().unwrap().as_ref().expect("アプリケーションのパスが初期化されていません").join(app_paths::JSON_FILE);
+
+        let mut data = JSON_DATA.lock().unwrap();
+        let json_str = std::fs::read_to_string(json_path).expect("JSONファイルの読み込みに失敗しました");
+        *data = serde_json::from_str(&json_str).expect("JSONのパースに失敗しました");
+
+        println!("JSONデータの初期化完了: {:?}", data);
+    }
+}
+
+
+
+// スペルとパスを保存するJSONの構造体の定義
+// #[derive(Serialize, Deserialize, Debug, Clone)]  // Cloneを追加しておくと便利
+// pub struct Entry {
+//     pub spell: String,
+//     pub path: String,
+// }
 
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,10 @@
 use tauri::tray::TrayIconBuilder;
 
-use std::sync::{Arc, Mutex};
-
 mod listen_keybord;
 mod get_directories;
 
 mod config;
-use config::AppState;
-
+use crate::config::json_data;
 mod count_keys;
 
 
@@ -15,15 +12,14 @@ mod count_keys;
 pub fn run() {
     tauri::Builder::default()
         .setup(|app|{
-            let _ = get_directories::get_directory();
+            get_directories::get_directory();
+            json_data::initialize_json_data();
             let _tray = TrayIconBuilder::new()
                 .icon(app.default_window_icon().unwrap().clone())
                 .build(app)?;
             Ok(())
         })
-        .manage(AppState { // AppStateを手動で初期化する必要がある、そのためArcやMutexを使っている
-            listener_process: Arc::new(Mutex::new(None)),
-        })
+        // .manage()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![listen_keybord::start_listening, listen_keybord::stop_listening])
         .run(tauri::generate_context!())
